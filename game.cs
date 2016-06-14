4967211f07b935b03071fa4d4dc97cd748dcc7be
Laura Tigchelaar <laura_tigchelaar@msn.com>
2016-06-14 11:33:49 +0200

    added post processing

--- game.cs
+++ game.cs
@@ -1,80 +1,100 @@
 ﻿using System.Diagnostics;
 using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 // minimal OpenTK rendering framework for UU/INFOGR
 // Jacco Bikker, 2016
 
-namespace Template_P3 {
-
-class Game
+namespace Template_P3
 {
-	// member variables
-	public Surface screen;					// background surface for printing etc.
-	Mesh mesh, floor, ding, klok_basis;						// a mesh to draw using OpenGL
-	const float PI = 3.1415926535f;			// PI
-	float a = 0, b = 0;							// teapot rotation angle
-	Stopwatch timer;						// timer for measuring frame duration
-	Shader shader;							// shader to use for rendering
-	Texture wood, paint, wol;							// texture to use for rendering
 
-	// initialize
-	public void Init()
-	{
-		// load teapot
-		mesh = new Mesh( "../../assets/teapot.obj" );
-		floor = new Mesh( "../../assets/floortest2.obj" );
-        ding = new Mesh("../../assets/objectje.obj");
-        klok_basis = new Mesh("../../assets/klok_basis.obj");
+    class Game
+    {
+        // member variables
+        public Surface screen;                  // background surface for printing etc.
+        Mesh mesh, floor, ding, klok_basis;                       // a mesh to draw using OpenGL
+        const float PI = 3.1415926535f;         // PI
+        float a = 0;                            // teapot rotation angle
+        Stopwatch timer;                        // timer for measuring frame duration
+        Shader shader;                          // shader to use for rendering
+        Shader postproc;                        // shader to use for post processing
+        Texture wood, paint, wol;                           // texture to use for rendering
+        RenderTarget target;                    // intermediate render target
+        ScreenQuad quad;                        // screen filling quad for post processing
+        bool useRenderTarget = true;
 
-		// initialize stopwatch
-		timer = new Stopwatch();
-		timer.Reset();
-		timer.Start();
-		// create shaders
-		shader = new Shader( "../../shaders/vs.glsl", "../../shaders/fs.glsl" );
-		// load a texture
-		wood = new Texture( "../../assets/wood.jpg" );
-        paint = new Texture("../../assets/paint.jpg");
-        wol = new Texture("../../assets/wol.jpg");
-	}
+        // initialize
+        public void Init()
+        {
+            // load teapot
+            mesh = new Mesh("../../assets/teapot.obj");
+            floor = new Mesh("../../assets/floortest2.obj");
+                    //ding = new Mesh("../../assets/objectje.obj");
+            klok_basis = new Mesh("../../assets/klok_basis.obj");
+            // initialize stopwatch
+            timer = new Stopwatch();
+            timer.Reset();
+            timer.Start();
+            // create shaders
+            shader = new Shader("../../shaders/vs.glsl", "../../shaders/fs.glsl");
+            postproc = new Shader("../../shaders/vs_post.glsl", "../../shaders/fs_post.glsl");
+            // load a texture
+            wood = new Texture("../../assets/wood.jpg");
+            paint = new Texture("../../assets/paint.jpg");
+            wol = new Texture("../../assets/wol.jpg");
+            // create the render target
+            target = new RenderTarget(screen.width, screen.height);
+            quad = new ScreenQuad();
+        }
 
-	// tick for background surface
-	public void Tick()
-	{
-		screen.Clear( 0 );
-	}
+        // tick for background surface
+        public void Tick()
+        {
+            screen.Clear(0);
+        }
 
-	// tick for OpenGL rendering code
-	public void RenderGL()
-	{
-		// measure frame duration
-		float frameDuration = timer.ElapsedMilliseconds;
-		timer.Reset();
-		timer.Start();
-	
-		// prepare matrix for vertex shader
-		Matrix4 transform = Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0 ), a );
-        Matrix4 transformfloor = Matrix4.CreateFromAxisAngle(new Vector3(1, 0, 0), a);
+        // tick for OpenGL rendering code
+        public void RenderGL()
+        {
+            // measure frame duration
+            float frameDuration = timer.ElapsedMilliseconds;
+            timer.Reset();
+            timer.Start();
+
+            // prepare matrix for vertex shader
+            Matrix4 transform = Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
+            Matrix4 transformfloor = Matrix4.CreateFromAxisAngle(new Vector3(1, 0, 0), a);
+
+            transform *= Matrix4.CreateTranslation(0, -4, -15);
+            transform *= Matrix4.CreatePerspectiveFieldOfView(1.2f, 1.3f, .1f, 1000);
 
             transformfloor *= transform;
 
-        transform *= Matrix4.CreateTranslation(0, -4, -15); // BEPAAL DE LOCATIE van het midden van het object
-        transform *= Matrix4.CreatePerspectiveFieldOfView(1.3f, 1.3f, .1f, 1000);
-
-        transformfloor *= Matrix4.CreateTranslation(0, -4, -35);
-        transformfloor *= Matrix4.CreatePerspectiveFieldOfView(1.3f, 1.3f, .1f, 1000);
-            
-
             // update rotation
-            a += 0.001f * frameDuration; 
-		if (a > 2 * PI) a -= 2 * PI;
-            // render scene
-        klok_basis.Render( shader, transform, wol );        
-		floor.Render( shader, transformfloor, paint );
-        //ding.Render(shader, transform, paint);
+            a += 0.001f * frameDuration;
+            if (a > 2 * PI) a -= 2 * PI;
 
-        // roep hier de sceneGraph aan met de door userinput aangepaste cameramatrix!
-	}
-}
+            if (useRenderTarget)
+            {
+                // enable render target
+                target.Bind();
+
+                // render scene to render target
+                klok_basis.Render(shader, transform, paint);
+                floor.Render(shader, transformfloor, paint);
+
+                // render quad
+                target.Unbind();
+                quad.Render(postproc, target.GetTextureID());
+            }
+            else
+            {
+                // render scene directly to the screen
+                klok_basis.Render(shader, transform, paint);
+                floor.Render(shader, transformfloor, paint);
+            }
+        }
+    }
 
 } // namespace Template_P3
